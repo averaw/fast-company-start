db5522cd7085ad8f7d7c66425db6468e7d5c7683
Eleonora Prokushenkova <eleonoraprokushenkova@MacBook-Air-Eleonora.local>
2022-04-18 13:47:05 +0300

    фильтрация

---
+++ src/app/components/groupList.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const GroupList = ({
+    items,
+    valueProperty,
+    contentProperty,
+    onItemSelect,
+    selectedItem
+}) => {
+    const isArray = Array.isArray(items);
+    return (
+        <ul className="list-group">
+            {!isArray
+                ? Object.keys(items).map((item) => (
+                    <li
+                        key={items[item][valueProperty]}
+                        className={
+                            "list-group-item" +
+                            (items[item] === selectedItem ? " active" : "")
+                        }
+                        onClick={() => onItemSelect(items[item])}
+                        role="button"
+                    >
+                        {items[item][contentProperty]}
+                    </li>
+                ))
+                : items.map((item) => {
+                    return (
+                        <li
+                            onClick={() => onItemSelect(item)}
+                            role="button"
+                            key={item._id}
+                            className={
+                                "list-group-item" +
+                                (item === selectedItem ? "active" : "")
+                            }
+                        >
+                            {item.name}
+                        </li>
+                    );
+                })}
+        </ul>
+    );
+};
+GroupList.defaultProps = {
+    valueProperty: "_id",
+    contentProperty: "name"
+};
+GroupList.propTypes = {
+    items: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
+    valueProperty: PropTypes.string.isRequired,
+    contentProperty: PropTypes.string.isRequired,
+    onItemSelect: PropTypes.func,
+    selectedItem: PropTypes.object
+};
+export default GroupList;
---
+++ src/app/components/usersTable.jsx
@@ -0,0 +1,30 @@
+/*import React from "react";
+import PropTypes from "prop-types";
+const UserTable = ({ users, ...rest }) => {
+    return (
+        <table className="table">
+            <thead>
+                <tr>
+                    <th scope="col">Имя</th>
+                    <th scope="col">Качества</th>
+                    <th scope="col">Провфессия</th>
+                    <th scope="col">Встретился, раз</th>
+                    <th scope="col">Оценка</th>
+                    <th scope="col">Избранное</th>
+                    <th />
+                </tr>
+            </thead>
+            <tbody>
+                {users.map((user) => (
+                    <User {...rest} {...user} key={user._id} />
+                ))}
+            </tbody>
+        </table>
+    );
+};
+
+UserTable.propTypes = {
+    users: PropTypes.array.isRequired
+};
+
+export default UserTable;*/
